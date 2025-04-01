909f3ac1b5f9fef25d129f0dadeb29d2037667fb
Radhika17102001 <radhika.lnu@rdpolytech.ca>
2025-04-01 18:38:38 -0500

    update

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const userRoutes = require('./routes/users.js');
 require('dotenv').config();
 const PORT = process.env.PORT;
 
+const rateLimit = require('express-rate-limit');
+const fixedWindowRateLimit = rateLimit({
+    windowsMs: 1* 15* 1000,
+    max: 10,
+    message: 'Too many request try later;)',
+});
 
 // set up the new engine
 app.set('view engine','ejs');
@@ -11,7 +17,9 @@ app.set('views','./views');
 
 //middleware to serve static files
 app.use(express.static('public'));
+app.use(fixedWindowRateLimit);
 app.use(userRoutes);
+
 
 app.listen(PORT,()=>{
     console.log(`connected on port: ${PORT}`);
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {createUser}= require('../controllers/userControl.js')
+const {createUser}= require('../controllers/userController.js')
 
 
 //Router handler for our home page
